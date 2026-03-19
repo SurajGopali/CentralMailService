a395940bfc30198ea278c039ec53e8558c99d918
SurajNeo <suraj.gopali@neoassure.com>
2026-03-19 12:10:30 +0545

    email file attachment

--- CentralMailService/Controllers/ApiController/EmailApiController.cs
+++ CentralMailService/Controllers/ApiController/EmailApiController.cs
@@ -18,7 +18,7 @@ namespace CentralMailService.Controllers.ApiController
         }
 
         [HttpPost("SendEmail")]
-        public async Task<IActionResult> SendEmail([FromBody] SendEmailRequest request)
+        public async Task<IActionResult> SendEmail([FromForm] SendEmailRequest request)
         {
             var apiKey = Request.Headers["x-api-key"].FirstOrDefault();
             if (string.IsNullOrEmpty(apiKey))
@@ -27,6 +27,29 @@ namespace CentralMailService.Controllers.ApiController
             var product = await _context.Products.FirstOrDefaultAsync(p => p.ApiKey == apiKey && p.IsActive);
             if (product == null)
                 return Unauthorized("Invalid API key");
+
+            string attachmentPath = null;
+            string attachmentName = null;
+
+            if (request.Attachment != null)
+            {
+                var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads");
+
+                if(!Directory.Exists(uploadsFolder))
+                    Directory.CreateDirectory(uploadsFolder);
+
+                var uniqueFileName = Guid.NewGuid() + "_" + request.Attachment.FileName;
+
+                var filePath = Path.Combine(uploadsFolder , uniqueFileName);
+
+                using( var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await request.Attachment.CopyToAsync(stream);   
+                }
+
+                attachmentPath = "uploads/" + uniqueFileName;
+                attachmentName = request.Attachment.FileName;
+            }
 
             var email = new EmailQueue
             {
@@ -37,6 +60,9 @@ namespace CentralMailService.Controllers.ApiController
                 Subject = request.Subject,
                 Body = request.Body,
                 Signature = request.Signature,
+                AttachmentName = attachmentName,
+                AttachmentPath = attachmentPath
+
             };
 
             _context.EmailQueue.Add(email);
